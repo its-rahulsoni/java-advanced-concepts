18901f64a2e758118736a78c2ec5bef34ded6e42
Rahul Soni <rahulsoni.myventures@gmail.com>
2025-06-02 14:21:53 +0530

    shared reader write problem

--- src/main/java/multithreading/reader_write_problem/SharedData.java
+++ src/main/java/multithreading/reader_write_problem/SharedData.java
@@ -5,6 +5,23 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 // Shared resource
 class SharedData {
     private int data = 0;  // Simulated shared resource
+
+    /**
+     * ReentrantReadWriteLock class of Java is an implementation of ReadWriteLock, that also supports ReentrantLock functionality.
+     *
+     * The ReadWriteLock is a pair of associated locks, one for read-only operations and one for writing. Whereas, the ReentrantLock is a re-entrant mutual exclusion Lock with the same behavior as the implicit monitor lock accessed using synchronized methods and statements, but with some more extended capabilities.
+     *
+     * ReadWriteLock in Java
+     *
+     * Even in a multi-threading application, multiple reads can occur simultaneously for a shared resource. It is only when multiple writes happen simultaneously or intermix of read and write that there is a chance of writing the wrong value or reading the wrong value.
+     *
+     * ReadWriteLock in Java uses the same idea in order to boost the performance by having separate pair of locks. A ReadWriteLock maintains a pair of associated locks-
+     *
+     * One for read-only operations; and
+     * one for writing.
+     * The read lock may be held simultaneously by multiple reader threads, so long as there are no writers. The write lock is exclusive.
+     * @param readerName
+     */
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     // Reader thread reads the data
